426e8ef63888af233efd514389fa9e98c51a1724
TannerMantooth <137127335+TannerMantooth@users.noreply.github.com>
2023-12-03 02:28:06 -0600

    Final commit with comments and updated pug files
    Code comments have been added to the app. All pug files have been updated to dynamically render the correct sources. Removed all unnecessary lines of code. Fixed issue with image files not being correctly imported for rendering.

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+//Import all required dependencies
 const express = require("express");
 const { projects } = require("./data.json");
 const app = express();
 
+//Setting the view engine and static method
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
 
+//Main route that renders index.pug
 app.get("/", (req, res) => {
   res.render("index", { projects });
 });
 
+//About route that renders about.pug
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
+//Dyanmic Project route that renders the project.pug template
 app.get("/projects/:id", (req, res) => {
   const projectId = req.params.id;
   const project = projects.find(({ id }) => id === +projectId);
@@ -26,10 +31,24 @@ app.get("/projects/:id", (req, res) => {
   }
 });
 
+//404 Error handler
 app.use((req, res, next) => {
   res.status(404).render("page-not-found");
 });
 
+//Global Error Handler
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).render("page-not-found", { err });
+  } else {
+    err.message =
+      err.message ||
+      "Oh no! It looks like something went wrong with the server.";
+    res.status(err.status || 500).render("error", { err });
+  }
+});
+
+//Initiating server and setting port to 3000
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!");
 });
